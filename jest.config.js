be9b7724d7fa0502abf6589ee59e7b0d61a0aabf
Marharyta <rita.shyshkavets@gmail.com>
2025-09-04 15:56:43 +0200

    chore: configure project setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/main.tsx'],
+  coverageDirectory: 'coverage',
+}
