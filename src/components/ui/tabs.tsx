b6e8a917ad1075c016e972fad8f35ceba8d1e6e8
Marharyta <rita.shyshkavets@gmail.com>
2025-09-08 13:22:51 +0200

    refactor: modernize components and replace React.Something with imports

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -2,62 +2,77 @@
 
 import * as React from 'react';
 import * as TabsPrimitive from '@radix-ui/react-tabs';
-
 import cn from '@/lib/utils';
+import { ComponentProps } from 'react';
 import Typography from '../Typography';
 
-const Tabs = TabsPrimitive.Root;
+function Tabs({
+  className,
+  ...props
+}: ComponentProps<typeof TabsPrimitive.Root>) {
+  return (
+    <TabsPrimitive.Root
+      data-slot="tabs"
+      className={cn('flex flex-col gap-2', className)}
+      {...props}
+    />
+  );
+}
 
-const TabsList = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.List>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={cn(
-      'text-muted-foreground inline-flex h-10 items-center justify-center gap-1 rounded-lg bg-[var(--primary)] p-2',
-      className,
-    )}
-    {...props}
-  />
-));
-TabsList.displayName = TabsPrimitive.List.displayName;
+function TabsList({
+  className,
+  ...props
+}: ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List
+      data-slot="tabs-list"
+      className={cn(
+        'text-muted-foreground inline-flex h-10 items-center justify-center gap-1 rounded-lg bg-[var(--primary)] p-2',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
 
-const TabsTrigger = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      'inline-flex items-center justify-center rounded-md px-3 py-1 whitespace-nowrap text-[var(--white)]',
-      'transition-colors duration-200',
-      'focus-visible:ring-ring focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none',
-      'disabled:pointer-events-none disabled:opacity-50',
-      'hover:bg-[var(--white)] hover:text-[var(--dark)]',
-      'data-[state=active]:bg-[var(--white)] data-[state=active]:text-[var(--dark)] data-[state=active]:shadow',
-      className,
-    )}
-    {...props}
-  >
-    <Typography variant="caption">{children}</Typography>
-  </TabsPrimitive.Trigger>
-));
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+function TabsTrigger({
+  className,
+  children,
+  ...props
+}: ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      data-slot="tabs-trigger"
+      className={cn(
+        'inline-flex items-center justify-center rounded-md px-3 py-1 whitespace-nowrap text-[var(--white)]',
+        'transition-colors duration-200',
+        'focus-visible:ring-ring focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none',
+        'disabled:pointer-events-none disabled:opacity-50',
+        'hover:bg-[var(--white)] hover:text-[var(--dark)]',
+        'data-[state=active]:bg-[var(--white)] data-[state=active]:text-[var(--dark)] data-[state=active]:shadow',
+        className,
+      )}
+      {...props}
+    >
+      <Typography variant="caption">{children}</Typography>
+    </TabsPrimitive.Trigger>
+  );
+}
 
-const TabsContent = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      'ring-offset-background focus-visible:ring-ring mt-2 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none',
-      className,
-    )}
-    {...props}
-  />
-));
-TabsContent.displayName = TabsPrimitive.Content.displayName;
+function TabsContent({
+  className,
+  ...props
+}: ComponentProps<typeof TabsPrimitive.Content>) {
+  return (
+    <TabsPrimitive.Content
+      data-slot="tabs-content"
+      className={cn(
+        'ring-offset-background focus-visible:ring-ring mt-2 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
 
 export { Tabs, TabsList, TabsTrigger, TabsContent };
