2c8808cc6047c8b3aa4ce434908db8b3fda14239
Yuliya Naletskaya <naletskaya@gmail.com>
2025-09-09 19:21:51 +0300

    fix: fix after merge

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,11 @@
+import AppWrapper from '@/components/app-wrapper';
 import AuthForm from '@/components/AuthForm';
-import Layout from '@/components/Layout';
-import Typography from '@/components/Typography';
+import Typography from '@/components/typography';
 
 export default function RegisterPage() {
   console.log('SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
   return (
-    <Layout>
+    <AppWrapper>
       <div className="flex h-full items-center justify-center">
         <div className="flex h-[408px] w-[550px] flex-col self-center rounded-[20px] bg-[var(--white)] px-16 py-8 text-center shadow-[0px_0px_11.6px_5px_rgba(0,0,0,0.08)]">
           <Typography
@@ -17,6 +17,6 @@ export default function RegisterPage() {
           <AuthForm mode="register" />
         </div>
       </div>
-    </Layout>
+    </AppWrapper>
   );
 }
