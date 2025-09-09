b9156151c911b7c00c4fe2dbd593f4a2d70cd161
Yuliya <91249561+YuliyaNaletskaya@users.noreply.github.com>
2025-09-09 21:25:01 +0300

    10 auth integration (#22)
    * feat: integrated with supabase and created service files
    
    * fix: fixed bug in tsconfig
    
    * fix: change function name and fix path
    
    * fix: change file name and types
    
    * refactor: remove unused function getEnv
    
    * Revert "fix: setup dependencies"
    
    This reverts commit 2b13c12d7fdefb26ae92f142d09c8e9af741676f.

---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+
+export async function createSupabaseServerClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch {
+            // Ignored: running in Server Component
+          }
+        },
+      },
+    },
+  );
+}
