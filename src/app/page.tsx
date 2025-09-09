423f239c15d4993fb92a49d03e5374c214d9f2c2
Marharyta <rita.shyshkavets@gmail.com>
2025-09-09 18:22:22 +0200

    feat: add project, course and team sections to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import AppWrapper from '@/components/app-wrapper';
-import Link from 'next/link';
-import Typography from '@/components/typography';
+import Hero from '@/components/hero';
+import Team from '@/components/team';
 
 function Home() {
   return (
     <AppWrapper>
-      <Link href="/ui-demo">
-        <Typography variant="body" className="flex justify-center">
-          To UI demo page
-        </Typography>
-      </Link>
+      <Hero />
+      <Team />
     </AppWrapper>
   );
 }
