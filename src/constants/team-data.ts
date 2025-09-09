15c87e55c4578c997ca1813282118f02152f4887
Marharyta <rita.shyshkavets@gmail.com>
2025-09-09 18:20:40 +0200

    feat: add team information section

---
+++ src/constants/team-data.ts
@@ -0,0 +1,25 @@
+const teamMembers = [
+  {
+    name: 'Marharyta Shyshkavets',
+    bio: 'Marharyta Shyshkavets is an aspiring frontend developer with a strong eye for detail and a passion for creating clean, responsive interfaces. Over the past year, she has been exploring web development, focusing on layout, styling, and building user-friendly designs. She enjoys working in collaborative environments, values open communication, and believes teamwork helps her bring out the best in her work.',
+    image: '/members/Marharyta-Shyshkavets.jpg',
+    github: 'https://github.com/margomint',
+    linkedin: 'https://www.linkedin.com/in/marharyta-shyshkavets-18b866363/',
+  },
+  {
+    name: 'Elena Pavlovich',
+    bio: 'Elena Pavlovich is an aspiring frontend developer with a keen eye for detail and a passion for building clean, responsive user interfaces. Over the past year, she has been diving into web development, focusing on layout, styling, and creating intuitive, user-friendly designs. She thrives in collaborative environments, values open communication, and believes that teamwork inspires her to achieve her best work.',
+    image: '/members/Elena-Pavlovich.jpg',
+    github: 'https://github.com/elena-web',
+    linkedin: 'https://www.linkedin.com/in/elena-pavlovich-0a3491232/',
+  },
+  {
+    name: 'Yuliya Naletskaya',
+    bio: 'Yuliya Naletskaya  is an aspiring frontend developer who recently transitioned into IT after completing a web design course in 2024 and a front-end development course at RS School in 2025. She is fascinated by the process of building modern web applications and intuitive user interfaces. Passionate about continuous learning, she embraces the fast-evolving nature of frontend development and enjoys applying new technologies to create up-to-date solutions.',
+    image: '/members/Yuliya-Naletskaya.jpg',
+    github: 'https://github.com/yuliyanaletskaya',
+    linkedin: 'https://www.linkedin.com/in/yuliya-naletskaya-08140698/',
+  },
+];
+
+export default teamMembers;
