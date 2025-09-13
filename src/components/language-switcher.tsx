57ca74231ca22175b6afdf26c720085ac15796b3
Marharyta <rita.shyshkavets@gmail.com>
2025-09-13 16:03:57 +0200

    feat: add LanguageSwitcher component for switching languages

---
+++ src/components/language-switcher.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Link, usePathname } from '@/i18n/navigation';
+import Typography from './typography';
+import { routing } from '@/i18n/routing';
+
+function LanguageSwitcher() {
+  const pathname = usePathname();
+
+  const currentRoute =
+    (Object.keys(routing.pathnames) as (keyof typeof routing.pathnames)[]).find(
+      (route) => pathname.startsWith(route),
+    ) || '/';
+
+  return (
+    <Typography variant="caption" className="flex gap-2 text-[var(--primary)]">
+      <Link
+        href={currentRoute}
+        locale="ru"
+        className="hover:text-[var(--gray-400)]"
+      >
+        RU
+      </Link>
+      |
+      <Link
+        href={currentRoute}
+        locale="en"
+        className="hover:text-[var(--gray-400)]"
+      >
+        EN
+      </Link>
+    </Typography>
+  );
+}
+
+export default LanguageSwitcher;
